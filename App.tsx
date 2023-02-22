cf720d7009e53dea1cdaa71cb61de807ccc7b53a
Elie Bracha <eliebrachapro@gmail.com>
2023-02-22 17:01:45 +0200

    styles

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
-export default function App() {
+const App = () =>{
   return (
-    <View>
+    <View style = {styles.container}>
       <Text>App Elie</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',  }
+})
+export default App
